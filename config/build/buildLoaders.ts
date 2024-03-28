b3d668e0eca12faa4ea978caf084685dface8f3a
oltree <oleg.melekh.frontend@gmail.com>
2024-03-28 20:44:19 +0300

    feat: add file and svg laoder, custom buttom component

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -30,5 +30,19 @@ export const buildLoaders = ({
     ],
   };
 
-  return [typescriptLoader, cssLoader];
+  const svgLoader = {
+    test: /\.svg$/,
+    use: ['@svgr/webpack'],
+  };
+
+  const fileLoader = {
+    test: /\.(png|jpe?g|gif|woff2|woff)$/i,
+    use: [
+      {
+        loader: 'file-loader',
+      },
+    ],
+  };
+
+  return [svgLoader, fileLoader, typescriptLoader, cssLoader];
 };
