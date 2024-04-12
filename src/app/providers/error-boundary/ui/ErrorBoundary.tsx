5ec9cb474b462ee6fd85338b71da79b3da3ecf67
oltree <oleg.melekh.frontend@gmail.com>
2024-04-12 18:59:18 +0300

    feat: add error boundary and page error

---
+++ src/app/providers/error-boundary/ui/ErrorBoundary.tsx
@@ -0,0 +1,42 @@
+import { Component, ErrorInfo, PropsWithChildren, Suspense } from 'react';
+
+import { PageError } from 'widgets/page-error';
+
+interface ErrorBoundaryState {
+  hasError: boolean;
+}
+
+class ErrorBoundary extends Component<PropsWithChildren, ErrorBoundaryState> {
+  constructor(props: PropsWithChildren) {
+    super(props);
+
+    this.state = { hasError: false };
+  }
+
+  static getDerivedStateFromError(/* error: Error */) {
+    // Update state so the next render will show the fallback UI.
+    return { hasError: true };
+  }
+
+  componentDidCatch(error: Error, errorInfo: ErrorInfo) {
+    // Update state so the next render will show the fallback UI.
+    console.error(error, errorInfo);
+  }
+
+  render() {
+    const { hasError } = this.state;
+    const { children } = this.props;
+
+    if (hasError) {
+      return (
+        <Suspense fallback=''>
+          <PageError />
+        </Suspense>
+      );
+    }
+
+    return children;
+  }
+}
+
+export default ErrorBoundary;
