a5d2d686c6db4585fca3d3417e8e2d0e53529cc5
oltree <oleg.melekh.frontend@gmail.com>
2024-05-01 22:43:07 +0300

    fix: fix global styles

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,16 +1,13 @@
 import { Suspense } from 'react';
 
 import { Router } from 'app/providers/router';
-import { useTheme } from 'app/providers/theme';
 import { classNames } from 'shared/lib/classNames/classNames';
 import { Navbar } from 'widgets/navbar';
 import { Sidebar } from 'widgets/sidebar';
 
 function App() {
-  const { theme } = useTheme();
-
   return (
-    <div className={classNames('app', {}, [theme])}>
+    <div className={classNames('app', {}, [])}>
       <Suspense fallback=''>
         <Navbar />
 
