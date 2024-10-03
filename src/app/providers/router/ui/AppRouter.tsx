2b51bd928344eb4c8e4da32d21f72581b74077d3
oleg melekh <oleg.melekh.frontend@gmail.com>
2024-10-03 08:22:09 +0300

    feat: refactoring FSD and use RTK

--- src/app/providers/router/ui/AppRouter.tsx
+++ src/app/providers/router/ui/AppRouter.tsx
@@ -1,30 +1,29 @@
-import React, { memo, Suspense, useCallback } from 'react';
-import { Route, Routes } from 'react-router-dom';
-import { PageLoader } from 'widgets/PageLoader/PageLoader';
-import { AppRoutesProps, routeConfig } from 'shared/config/routeConfig/routeConfig';
 import { RequireAuth } from 'app/providers/router/ui/RequireAuth';
+import { memo, Suspense, useCallback } from 'react';
+import { Route, Routes } from 'react-router-dom';
+import {
+  AppRoutesProps,
+  routeConfig,
+} from 'shared/config/routeConfig/routeConfig';
+import { PageLoader } from 'widgets/PageLoader/PageLoader';
 
 const AppRouter = () => {
-    const renderWithWrapper = useCallback((route: AppRoutesProps) => {
-        const element = (
-          <Suspense fallback={<PageLoader />}>
-            {route.element}
-          </Suspense>
-        );
-        return (
-          <Route
-            key={route.path}
-            path={route.path}
-            element={route.authOnly ? <RequireAuth>{element}</RequireAuth> : element}
-          />
-        );
-    }, []);
-
-    return (
-      <Routes>
-        {Object.values(routeConfig).map(renderWithWrapper)}
-      </Routes>
+  const renderWithWrapper = useCallback((route: AppRoutesProps) => {
+    const element = (
+      <Suspense fallback={<PageLoader />}>{route.element}</Suspense>
     );
+    return (
+      <Route
+        key={route.path}
+        path={route.path}
+        element={
+          route.authOnly ? <RequireAuth>{element}</RequireAuth> : element
+        }
+      />
+    );
+  }, []);
+
+  return <Routes>{Object.values(routeConfig).map(renderWithWrapper)}</Routes>;
 };
 
 export default memo(AppRouter);
--- src/widgets/sidebar/ui/Sidebar/Sidebar.tsx
+++ src/widgets/sidebar/ui/Sidebar/Sidebar.tsx
@@ -1,60 +1,58 @@
-import { classNames } from 'shared/lib/classNames/classNames';
 import { memo, useMemo, useState } from 'react';
-import { ThemeSwitcher } from 'widgets/ThemeSwitcher';
-import { LangSwitcher } from 'widgets/LangSwitcher/LangSwitcher';
-import { Button, ButtonSize, ButtonTheme } from 'shared/ui/Button/Button';
 import { useSelector } from 'react-redux';
-import { getUserAuthData } from 'entities/User';
+import { classNames } from 'shared/lib/classNames/classNames';
+import { Button, ButtonSize, ButtonTheme } from 'shared/ui/Button/Button';
 import { VStack } from 'shared/ui/Stack/VStack/VStack';
-import cls from './Sidebar.module.scss';
-import { SidebarItem } from '../SidebarItem/SidebarItem';
+import { LangSwitcher } from 'widgets/LangSwitcher/LangSwitcher';
+import { ThemeSwitcher } from 'widgets/ThemeSwitcher';
 import { getSidebarItems } from '../../model/selectors/getSidebarItems';
+import { SidebarItem } from '../SidebarItem/SidebarItem';
+import cls from './Sidebar.module.scss';
 
 interface SidebarProps {
-    className?: string;
+  className?: string;
 }
 
 export const Sidebar = memo(({ className }: SidebarProps) => {
-    const [collapsed, setCollapsed] = useState(false);
-    const sidebarItemsList = useSelector(getSidebarItems);
+  const [collapsed, setCollapsed] = useState(false);
+  const sidebarItemsList = useSelector(getSidebarItems);
 
-    const onToggle = () => {
-        setCollapsed((prev) => !prev);
-    };
+  const onToggle = () => {
+    setCollapsed(prev => !prev);
+  };
 
-    const itemsList = useMemo(() => sidebarItemsList.map((item) => (
-      <SidebarItem
-        item={item}
-        collapsed={collapsed}
-        key={item.path}
-      />
-    )), [collapsed, sidebarItemsList]);
+  const itemsList = useMemo(
+    () =>
+      sidebarItemsList.map(item => (
+        <SidebarItem item={item} collapsed={collapsed} key={item.path} />
+      )),
+    [collapsed, sidebarItemsList]
+  );
 
-    return (
-      <aside
-        data-testid='sidebar'
-        className={classNames(cls.Sidebar, { [cls.collapsed]: collapsed }, [className])}
+  return (
+    <aside
+      data-testid='sidebar'
+      className={classNames(cls.Sidebar, { [cls.collapsed]: collapsed }, [
+        className,
+      ])}
+    >
+      <Button
+        data-testid='sidebar-toggle'
+        onClick={onToggle}
+        className={cls.collapseBtn}
+        theme={ButtonTheme.BACKGROUND_INVERTED}
+        size={ButtonSize.L}
+        square
       >
-        <Button
-          data-testid='sidebar-toggle'
-          onClick={onToggle}
-          className={cls.collapseBtn}
-          theme={ButtonTheme.BACKGROUND_INVERTED}
-          size={ButtonSize.L}
-          square
-        >
-          {collapsed ? '>' : '<'}
-        </Button>
-        <VStack role='navigation' gap='8' className={cls.items}>
-          {itemsList}
-        </VStack>
-        <div className={cls.switchers}>
-          <ThemeSwitcher />
-          <LangSwitcher
-            short={collapsed}
-            className={cls.lang}
-          />
-        </div>
-      </aside>
-    );
+        {collapsed ? '>' : '<'}
+      </Button>
+      <VStack role='navigation' gap='8' className={cls.items}>
+        {itemsList}
+      </VStack>
+      <div className={cls.switchers}>
+        <ThemeSwitcher />
+        <LangSwitcher short={collapsed} className={cls.lang} />
+      </div>
+    </aside>
+  );
 });
